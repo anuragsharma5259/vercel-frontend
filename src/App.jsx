4bbbb3ae131c6c2f251beb34a01637fc82c605b7
anuragsharma5259 <anuragsharma03857@gmail.com>
2025-03-09 02:30:16 +0530

    env

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
         formData.append("language", language);
     
         try {
-            const response = await fetch(`${process.env.BACKEND_URL}/upload-resume`, {
+            const response = await fetch(`https://roastapi-production.up.railway.app/upload-resume`, {
                 method: "POST",
                 body: formData,
             });
